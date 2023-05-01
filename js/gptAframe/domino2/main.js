b7fe7481e4a11a88f871be4c36175e4fd8e0d187
tmdoi <doi@osaka-pct.ac.jp>
2023-05-02 08:29:42 +0900

    Update main.js

--- js/gptAframe/domino2/main.js
+++ js/gptAframe/domino2/main.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", function () {
   const scene = document.querySelector("a-scene");
   const dominoes = document.getElementById("dominoes");
+  const timer = document.getElementById("timer");
 
   for (let i = 0; i < 100; i++) {
     const domino = document.createElement("a-entity");
@@ -10,6 +11,20 @@ window.addEventListener("DOMContentLoaded", function () {
   }
 
   const firstDomino = dominoes.children[0];
+  firstDomino.setAttribute("material", "color: #0000FF"); // 1つ目のドミノを青色に変更
+
+  function countdown(seconds) {
+    timer.setAttribute("text", "value", seconds);
+
+    if (seconds > 0) {
+      setTimeout(() => {
+        countdown(seconds - 1);
+      }, 1000);
+    }
+  }
+  
+  countdown(3);
+  
   setTimeout(() => {
     firstDomino.components["dynamic-body"].applyImpulse(
       new THREE.Vector3(0, 0, 5), // 方向を修正しました
