2bc64341863a0b0b4634a3a91a5bf1d0fa17d318
tmdoi <doi@osaka-pct.ac.jp>
2023-05-02 08:22:00 +0900

    Create main.js

---
+++ js/gptAframe/domino2/main.js
@@ -0,0 +1,19 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const scene = document.querySelector("a-scene");
+  const dominoes = document.getElementById("dominoes");
+
+  for (let i = 0; i < 100; i++) {
+    const domino = document.createElement("a-entity");
+    domino.setAttribute("mixin", "domino");
+    domino.setAttribute("position", `0 0.25 ${-0.2 * (i + 1)}`);
+    dominoes.appendChild(domino);
+  }
+
+  const firstDomino = dominoes.children[0];
+  setTimeout(() => {
+    firstDomino.components["dynamic-body"].applyImpulse(
+      new THREE.Vector3(0, 0, 5), // 方向を修正しました
+      new THREE.Vector3().copy(firstDomino.getAttribute("position"))
+    );
+  }, 3000);
+});
