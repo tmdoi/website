5cc07f22695834644f34ae2465936a91ba3523fe
tmdoi <doi@osaka-pct.ac.jp>
2023-05-05 18:39:43 +0900

    Create main.js

---
+++ js/gptAframe/domino4/main.js
@@ -0,0 +1,40 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const scene = document.querySelector("a-scene");
+  const dominoes = document.getElementById("dominoes");
+  const timer = document.getElementById("timer");
+
+  for (let i = 0; i < 100; i++) {
+    const domino = document.createElement("a-box");
+    domino.setAttribute("width", "0.25");
+    domino.setAttribute("height", "0.5");
+    domino.setAttribute("depth", "0.1");
+    domino.setAttribute("color", "#FFC107");
+    domino.setAttribute("position", `0 0.25 ${-0.2 * (i + 1)}`);
+    domino.setAttribute("dynamic-body", "shape: box; mass: 0.25; friction: 0.5");
+    dominoes.appendChild(domino);
+  }
+
+  const firstDomino = dominoes.children[0];
+  firstDomino.setAttribute("color", "#0000FF"); // 1つ目のドミノを青色に変更
+
+  function countdown(seconds) {
+    timer.setAttribute("text", "value", seconds);
+
+    if (seconds > 0) {
+      setTimeout(() => {
+        countdown(seconds - 1);
+      }, 1000);
+    }
+  }
+
+  countdown(3);
+
+  firstDomino.addEventListener("body-loaded", () => {
+    setTimeout(() => {
+      firstDomino.components["dynamic-body"].applyImpulse(
+        new THREE.Vector3(0, 0, 5),
+        new THREE.Vector3().copy(firstDomino.getAttribute("position"))
+      );
+    }, 3000);
+  });
+});
