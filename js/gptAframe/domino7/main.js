2ffb54f1ce1729e1d42bff51ce5d6dc3dfe90f0c
tmdoi <doi@osaka-pct.ac.jp>
2023-05-18 08:28:39 +0900

    Update main.js

--- js/gptAframe/domino7/main.js
+++ js/gptAframe/domino7/main.js
@@ -55,7 +55,7 @@ window.addEventListener("DOMContentLoaded", function () {
     return {x: posX, y: posY, z: posZ};
   }
 
-  let nextPos = createDominoes(100, 0, 0.25, 0, 0);
+  let nextPos = createDominoes(100, 0, 0.25, 0, 1);
 
   const firstDomino = dominoes.children[0];
   firstDomino.setAttribute("color", "#0000FF");
